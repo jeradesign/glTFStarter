03149d7b67bf9376769aecf210589867b3111dbe
John Brewer <jbrewer@jera.com>
2025-10-04 16:13:53 -0700

    Initial Commit

---
+++ glTFStarter/ImmersiveView.swift
@@ -0,0 +1,30 @@
+//
+//  ImmersiveView.swift
+//  glTFStarter
+//
+//  Created by John Brewer on 10/4/25.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ImmersiveView: View {
+
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(immersiveContentEntity)
+
+                // Put skybox here.  See example in World project available at
+                // https://developer.apple.com/
+            }
+        }
+    }
+}
+
+#Preview(immersionStyle: .mixed) {
+    ImmersiveView()
+        .environment(AppModel())
+}
---
+++ glTFStarter/glTFStarterApp.swift
@@ -0,0 +1,34 @@
+//
+//  glTFStarterApp.swift
+//  glTFStarter
+//
+//  Created by John Brewer on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct glTFStarterApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+        .windowStyle(.volumetric)
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+    }
+}
